5e8fe2772fbb61bc160543ee2453cda8f0fdb291
Edward <github@edwardcs.com>
2022-11-12 17:41:41 +0200

    Add text content to homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,7 +1,7 @@
 import HandPizza from "./imgs/hand-holding-pizza.jpg";
-import PizzaBites from "./imgs/pizza-bites.jpg";
 import IceCream from "./imgs/ice-cream.jpg";
 import RestaurantInside from "./imgs/restaurant-inside.jpg";
+import RestaurantTeam from "./imgs/restaurant-team.jpg";
 
 const content = document.querySelector("#content");
 
@@ -50,56 +50,72 @@ function wrapTags(className = undefined, ...elements) {
  * Append all elements to home container
  */
 function addHomeContent() {
-  const firstImage = new Image(800, 600);
+  const firstImage = new Image(736, 500);
   firstImage.src = HandPizza;
 
-  const secondImage = new Image(800, 600);
+  const secondImage = new Image(736, 500);
   secondImage.src = IceCream;
 
-  const thirdImage = new Image(800, 600);
-  thirdImage.src = PizzaBites;
+  const thirdImage = new Image(736, 500);
+  thirdImage.src = RestaurantInside;
 
-  const fourthImage = new Image(800, 600);
-  fourthImage.src = RestaurantInside;
+  const fourthImage = new Image(736, 500);
+  fourthImage.src = RestaurantTeam;
 
-  const firstHeading = "Heading test1";
-  const firstParagraph = "paragraph test1";
+  const txt1H1 =
+    "It's time for you to try the greatest and finest pizza... that is also properly cooked.";
+  const txt1P1 =
+    "You know it, Edward's has elite fresh, juicy toppings and ingredients that make our delicious tasting pizza.";
 
-  const secondHeading = "Heading test2";
-  const secondParagraph = "paragraph test2";
+  const txt2H1 =
+    "People Don't Just Say Ice Cream, they say \"Edward's Ice Cream\"";
+  const txt2P1 =
+    "Edward's Vegan Ice Cream are the new standard by which all other vegan ice creams are judged against. Many have tried and failed, but there's nothing like the verified, original Edward's Vegan Ice Cream. Now with a new, limited time only, flavour, the \" Vegan Coconut Macadamia \"";
+  const txt2P2 =
+    "Available at participating Edward's® for a limited time only.";
 
-  const thirdHeading = "Heading test 3";
-  const thirdParagraph = "paragraph test3";
+  const txt3H1 = "We're bringing the joy to your taste buds";
+  const txt3P1 =
+    "Do you like a properly cooked pizza with elite ingredients? That was rhetorical… of course you do! Edward's is NOW OPEN!";
+  const txt3P2 =
+    "Find us in Cluj-Napoca, Berlin, Napoli, London, Bucharest, Kyiv, Budapest, Zagreb, Sofia and Warsaw.";
 
-  const fourthHeading = "Heading test 4";
-  const fourthParagraph = "paragraph test4";
+  const txt4H1 = "Real Food. Real Careers.";
+  const txt4P1 = "Come and work for Edward's!";
+  const txt4P2 =
+    "For 23 years we've been serving up our delicious pizzas and have remained devoted to treating our employees in that same spirit that we cook the food: with care and love. You know what you want, and we'll embrace that when you work here. Flexibility. Fun. The feeling that you can just be you.";
+  const txt4P3 = "No zero-contracts. No nonsense. Jobs and careers done right.";
 
   wrapTags("image-container img1", firstImage);
   wrapTags(
     "text-container txt1",
-    wrapText(firstHeading, "h", 1).newTag,
-    wrapText(firstParagraph, "p").newTag
+    wrapText(txt1H1, "h", 1).newTag,
+    wrapText(txt1P1, "p").newTag
   );
 
   wrapTags("image-container img2", secondImage);
   wrapTags(
     "text-container txt2",
-    wrapText(secondHeading, "h", 1).newTag,
-    wrapText(secondParagraph, "p").newTag
+    wrapText(txt2H1, "h", 1).newTag,
+    wrapText(txt2P1, "p").newTag,
+    wrapText(txt2P2, "p").newTag
   );
 
   wrapTags("image-container img3", thirdImage);
   wrapTags(
     "text-container txt3",
-    wrapText(thirdHeading, "h", 1).newTag,
-    wrapText(thirdParagraph, "p").newTag
+    wrapText(txt3H1, "h", 1).newTag,
+    wrapText(txt3P1, "p").newTag,
+    wrapText(txt3P2, "p").newTag
   );
 
   wrapTags("image-container img4", fourthImage);
   wrapTags(
     "text-container txt4",
-    wrapText(fourthHeading, "h", 1).newTag,
-    wrapText(fourthParagraph, "p").newTag
+    wrapText(txt4H1, "h", 1).newTag,
+    wrapText(txt4P1, "p").newTag,
+    wrapText(txt4P2, "p").newTag,
+    wrapText(txt4P3, "p").newTag
   );
 
   content.appendChild(homeContainer);
